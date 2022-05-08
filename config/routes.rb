1400277ca07ae867a991c988c3ccfc48498ddd3e
sohei0613 <sohei080613@gmail.com>
2022-05-08 11:25:13 +0000

    [add]投稿ページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     get "homes/about" => "homes#about", as: :about
     
-     resources :books, only: [:new, :index, :show]
-     resources :users, only: [:index, :show]
+     resources :books, only: [:new, :index, :show, :edit, :create]
+     resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2022_05_07_061423) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
